babf98bfc50c0344b042e518c9a3a824e852f95f
CM-Jin <amorfati101@naver.com>
2026-03-04 02:13:46 +0900

    service-worker.js 업데이트

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,7 @@
-self.addEventListener('install', e => {
-  console.log('🧶 Service Worker installed');
-});
-
-self.addEventListener('fetch', e => {
-  // 캐시 기능 필요 없으면 생략 가능
-});
-
-self.addEventListener('activate', e => {
-  console.log('🧶 Service Worker active');
+self.addEventListener('install', e=>{
+  e.waitUntil(
+    caches.open('counter-v1').then(cache=>{
+      return cache.addAll(['./','index.html']);
+    })
+  );
 });
